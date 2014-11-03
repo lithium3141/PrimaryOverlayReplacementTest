19a588bd2a05b1e0b74850fb6795a201cb65b2b5
Tim Ekl <lithium3141@gmail.com>
2014-11-03 09:26:48 -0800

    Initial Commit

---
+++ PrimaryOverlayReplacementTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PrimaryOverlayReplacementTest
+//
+//  Created by Tim Ekl on 2014.11.03.
+//  Copyright (c) 2014 The Omni Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PrimaryOverlayReplacementTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PrimaryOverlayReplacementTest
+//
+//  Created by Tim Ekl on 2014.11.03.
+//  Copyright (c) 2014 The Omni Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
